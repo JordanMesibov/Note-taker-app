550b5d1b60b18dc19faed4b4f6c00bfe1460c94e
shinyscyther <mesibovj@gmail.com>
2019-02-05 16:58:33 -0500

    set up the htmlRoutes.js file

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -5,12 +5,14 @@ router
   .route("/")
   .get(function(req, res) {
    // SEND HOME PAGE USING RES.SENDFILE AND PATH.JOIN()
+   res.sendFile(path.join(__dirname, "../../public/assets/home.html"));
   });
 
 router
   .route("/notes")
   .get(function (req, res) {
     // SEND NOTES PAGE USING RES.SENDFILE AND PATH.JOIN()
+    res.sendFile(path.join(__dirname, "../../public/assets/notes.html"));
   });
 
 module.exports = router;
